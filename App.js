82e429f82e24658a91d89b86172b5ff4999665a1
elana strijbis <elana.strijbis@99designs.com>
2023-01-15 18:19:36 +1000

    Add navigation

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import CategoriesScreen from './screens/CategoriesScreen';
 import { StatusBar } from 'expo-status-bar';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CategoriesScreen from './screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
